876582c5f1561ba6e8aa849c3b3d9fe3a7c9019a
Riccardo Persello <riccardo.persello@icloud.com>
2025-02-12 22:32:17 +0100

    Cleanup

--- Sources/CircuitKit/Complex/Int+Complex.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-// MARK: - Extend Int to have an imaginary unit generator
-extension Int {
-    public var j: Complex {
-        return Complex(real: 0, imaginary: Double(self))
-    }
-}
